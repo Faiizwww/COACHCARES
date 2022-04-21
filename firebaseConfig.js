78fa96f4aeb71c858442cb7b26e5c7ba4b03ed62
Faiizwww <104120633+Faiizwww@users.noreply.github.com>
2022-04-21 11:36:44 +0700

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9s3mdGunWfDEMcJzYhO6rMA6Ycr2580M",
+    authDomain: "coach-cares.firebaseapp.com",
+    projectId: "coach-cares",
+    storageBucket: "coach-cares.appspot.com",
+    messagingSenderId: "433982373301",
+    appId: "1:433982373301:web:1c95a96c13302bd48cadfc",
+    measurementId: "G-3TR8MXTC6C"
+  };
+firebase.initializeApp(firebaseConfig)
